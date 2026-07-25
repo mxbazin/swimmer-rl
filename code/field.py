2b2643921ddd68ffc7fb3cc2a2ca4377c89b9cd2
maxime bazin <mbazin@umich.edu>
2026-07-25 10:30:00 -0400

    env: swim + clip of the TG domain

--- code/field.py
+++ code/field.py
@@ -25,24 +25,33 @@ Y = np.linspace(0, 2*np.pi, 50)
 x, y = np.meshgrid(X, Y, indexing='xy')
 U, V = taylor_green_field(x, y)
 
-
 if __name__ == "__main__":
     print("Starting the script..")
 
     #Initial pos of the swimmer
     px, py = x0, y0
 
+    # Adding the swimming parameters
+    theta = np.pi/2
+    v_swim = 0.3
+
     # Compute the position of the swimmer
     for i in range (0, N):
         u, v = taylor_green_field(px, py)
 
+        # Swimming stuff 
+        u = u + v_swim*np.cos(theta)
+        v = v + v_swim*np.sin(theta)
+
         px, py = euler_method(px, py, u, v, dt)
+        px = np.clip(px, 0, 2*np.pi)
+        py = np.clip(py, 0, 2*np.pi)
         list_pos.append((px, py))
 
     #Plot TG field + trajectory of the swimmer
     traj = np.array(list_pos)
     fig, ax = plt.subplots()
     q = ax.quiver(X, Y, U, V)
-    ax.quiverkey(q,X=1, Y=1, U=10, label ='Quiver, length =10', labelpos='E')
+    ax.quiverkey(q,X=1, Y=1, U=5, label ='Quiver, length =5', labelpos='E')
     plt.plot(traj[:,0], traj[:,1], 'r--')
     plt.show()
