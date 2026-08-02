ca180ecd3002e2c39eb7864675439769d4c083ca
maxime bazin <mbazin@umich.edu>
2026-08-02 13:05:25 -0400

    viz traj reinforce

--- code/viz.py
+++ code/viz.py
@@ -2,10 +2,14 @@
 
 import matplotlib.pyplot as plt 
 import statistics as stats 
-import random
+import torch
 import numpy as np
+from env import SwimmerEnv, taylor_green_field
+from policy import PolicyRL
 import glob
 import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
 
 def plot_returns(returns, window, title):
    mean_returns=[]
@@ -50,50 +54,60 @@ def plot_comparison(pattern, title, window=20, list_label=None, greedy_ref=None)
    plt.show()
 
 
-if __name__ == "__main__":
-   list_label = ["disc_104", "disc_171", "disc_97", "rtg_104", "rtg_171", "rtg_97"]
-   plot_comparison('runs/*nupdates300*.npy', 'vswim05_6runs', window=100, list_label=list_label, greedy_ref=-98.5)
-   # plot_comparison('runs/*seed97*.npy', 'rtg_seed97',window=20, list_label=None)
-   # plot_comparison('runs/*seed104*.npy', 'rtg_seed104',window=20, list_label=None)
-   # plot_comparison('runs/*seed171*.npy', 'rtg_seed171',window=20, list_label=None)
-
-   # plot_comparison('runs/*rtg*lr0.001*.npy', 'rtg_lr1e-3_3seeds' ,window=20, list_label=None)
-   # plot_comparison('runs/*discounted*lr0.001*.npy', 'discounted_lr1e-3_3seeds',window=20, list_label=None)
-   # plot_comparison('runs/*seed171*.npy','seed171_toutes_variantes',window=20, list_label=None)
-
-
-    #Plot TG field + trajectory of the swimmer
-    # traj = np.array(list_pos)
-    # fig, ax = plt.subplots()
-    # q = ax.quiver(X, Y, U, V)
-    # ax.quiverkey(q, X=1, Y=1, U=5, label =f'Quiver, length =5', labelpos='E')
-    # ax.scatter(traj[0,0], traj[0,1], c='b', marker='o', s=100, label='start')
-    # ax.scatter(env.target_x, env.target_y, c='g', marker='*', s=200, label='target')
-    # ax.set_xlabel('x')
-    # ax.set_ylabel('y')
-    # ax.set_aspect('equal')
-    # ax.plot(traj[:,0], traj[:,1], 'r--',label='trajectory')
-    # ax.set_title(f'v_swim= {env.v_swim}')
-    # ax.legend()
-    # fig.savefig(f'figs/traj_vswim_{env.v_swim}.png')
-    # plt.show()
-    # plt.close()
-
-    # #Animation
-    # fig2, ax2 = plt.subplots()
-    # q2 = ax2.quiver(X, Y, U, V)
-    # ax2.quiverkey(q2, X=1, Y=1, U=5, label =f'Quiver, length =5', labelpos='E')
-    # ax2.scatter(traj[0,0], traj[0,1], c='b', marker='o', s=100, label='start')
-    # ax2.scatter(env.target_x, env.target_y, c='g', marker='*', s=200, label='target')
-
-    # point, = ax2.plot([], [], 'ro')   # nageur, vide au départ
-    # trace, = ax2.plot([], [], 'r--')   # trace, vide au départ
-
-    # def update(frame):
-    #     point.set_data([traj[frame,0]], [traj[frame,1]])        
-    #     trace.set_data(traj[:frame, 0], traj[:frame, 1])        
-    #     return point, trace
+def plot_trajectory(list_pos, env, title, ax=None):
     
-    # anim = FuncAnimation(fig2, update, frames=range(0, len(traj), 5), interval=30)
-    # anim.save(f'figs/traj_vswim_{env.v_swim}.gif', writer='pillow')
-    # plt.show()
+    #Plot TG field + trajectory of the swimmer
+   if ax is None: 
+      fig, ax = plt.subplots()
+      standalone=True
+   else: 
+      standalone=False
+
+   X = np.linspace(env.domain_min, env.domain_max, 50)
+   Y = np.linspace(env.domain_min, env.domain_max, 50)
+   x, y = np.meshgrid(X, Y, indexing='xy')
+   U, V = taylor_green_field(x, y)
+
+   traj = np.array(list_pos)
+   q = ax.quiver(X, Y, U, V)
+   ax.scatter(traj[0,0], traj[0,1], c='b', marker='o', s=100, label='start')
+   ax.scatter(env.target_x, env.target_y, c='g', marker='*', s=200, label='target')
+   ax.set_xlabel('x')
+   ax.set_ylabel('y')
+   ax.set_aspect('equal')
+   ax.plot(traj[:,0], traj[:,1], 'r.',label='trajectory')
+   ax.set_title(f'{title} — {len(list_pos)} pas')
+   if standalone:
+      ax.legend()
+      ax.quiverkey(q, X=1, Y=1, U=5, label =f'Quiver, length =5', labelpos='E')
+      fig.savefig(f'figs/traj_vswim_{title}.png')
+      plt.show()
+      plt.close()
+
+if __name__ == "__main__":
+   env = SwimmerEnv(0.5, 0.1, shaping=True)
+   log_std_init = -1 ; squash=True
+   policy = PolicyRL(log_std_init, delta_max=env.delta_max, squash=squash)
+   paths = sorted(glob.glob('runs/reinforce/policy_*.pt'))
+   fig, axes = plt.subplots(2, 3, figsize=(15,10))
+   axes = axes.flatten()
+   list_pos=[]
+   
+   for path, ax in zip(paths, axes):
+         pts = torch.load(path)
+         policy.load_state_dict(pts)
+         obs = env.reset()
+         terminated = truncated = False
+         list_pos = []
+         while not (terminated or truncated):
+            action = policy.act(obs, deterministic=True)
+            obs, reward, terminated, truncated, info = env.step(action)
+            list_pos.append((env.px, env.py))  
+         plot_trajectory(list_pos, env, title=os.path.basename(path).replace('policy_mean_periodic_', '').replace('.pt', '').replace('nupdates300_log_std_init-1_use_criticTrue_lrcritic0.01_shapingTrue_squashTrue_baselineTrue_', ''), ax=ax)
+   fig.tight_layout()
+   plt.savefig('figs/traj_vswim_traj_6runs.png')
+   plt.show()
+
+   # list_label = ["disc_104", "disc_171", "disc_97", "rtg_104", "rtg_171", "rtg_97"]
+   # plot_comparison('runs/*nupdates300*.npy', 'vswim05_6runs', window=100, list_label=list_label, greedy_ref=-98.5)
+
